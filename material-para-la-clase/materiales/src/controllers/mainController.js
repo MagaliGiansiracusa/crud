23cf3c4618f78ef62380aa930eb4521b275f4d0b
MagaliGiansiracusa <giansiracusamagali@gmail.com>
2022-04-13 12:51:20 -0300

    cambios

--- material-para-la-clase/materiales/src/controllers/mainController.js
+++ material-para-la-clase/materiales/src/controllers/mainController.js
@@ -4,14 +4,20 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
+
+const inSale = products.filter( p => p.category === "in-sale");
+const visited = products.filter( p => p.category === "visited");
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	index: (req, res) => {
-		// Do the magic
+		res.render("index",{inSale, visited, toThousand});
 	},
 	search: (req, res) => {
-		// Do the magic
+		let search = req.query.keywords;
+		let findProducts = products.filter ( (p) => 
+			p.name.toLowerCase().includes(search.toLoweCase().trim()));
+			res.render("results", {products: findProducts, search, toThousand});
 	},
 };
 
